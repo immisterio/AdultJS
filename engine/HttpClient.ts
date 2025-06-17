f8ff7f866d4cf85c6d8f9580eb6ecafa871b87c2
immisterio <96652250+immisterio@users.noreply.github.com>
2025-06-17 15:27:55 +0300

    Native

--- engine/HttpClient.ts
+++ engine/HttpClient.ts
@@ -1,13 +1,35 @@
 export class HttpClient
 {
+    private static isAndroid: boolean = (() => {
+        if (typeof window !== 'undefined' &&
+            typeof (window as any).Lampa !== 'undefined' &&
+            typeof (window as any).Lampa.Platform !== 'undefined' &&
+            typeof (window as any).Lampa.Platform.is === 'function') {
+            return (window as any).Lampa.Platform.is('android');
+        }
+        return false;
+    })();
+
     public static ensureUserAgent(headers?: { [key: string]: string }): { [key: string]: string } {
         return headers ? { ...headers } : {};
     }
 
     public static async Get(url: string, headers?: { [key: string]: string }): Promise<string> {
+        if (HttpClient.isAndroid) 
+            return HttpClient.Native(url);
+
         const finalHeaders = HttpClient.ensureUserAgent(headers);
         const options: any = { method: 'GET', headers: finalHeaders };
         const response = await fetch(url, options);
         return await response.text();
+    }
+
+    public static Native(url: string, post?: boolean, params?: any): Promise<string> {
+        return new Promise(function (resolve, reject) {
+            (window as any).net["native"](url, function (result: any) {
+                if (typeof result === 'object') resolve(JSON.stringify(result));
+                else resolve(result);
+            }, reject, post, params);
+        });
     }
 }
