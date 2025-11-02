7a78f5677a6ac07cb5cdd99522d3e35de39d25d4
immisterio <96652250+immisterio@users.noreply.github.com>
2025-11-02 18:59:56 +0200

    fix

--- sisi.js
+++ sisi.js
@@ -433,7 +433,7 @@
       var _this = this;
 
       this.activity.loader(true);
-	  comp.render().find('category-full').addClass('mapping--grid cols--3');
+	  comp.render().find('.category-full').addClass('mapping--grid cols--3');
       Api.view(object, function(data) {
         menu = data.menu;
 
